9d7c7e3b345e954704d2afe3c5df22223eb00aba
usama khan <usama24khan@gmail.com>
2026-06-20 15:41:07 +0500

    vercel config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
-import dotenv from "dotenv";
-import path from "path";
-
-const appEnv = process.env.APP_ENV || (process.env.NODE_ENV === "development" ? "development" : "production");
-dotenv.config({
-  path: path.resolve(process.cwd(), `.env.${appEnv}`),
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
 export default nextConfig;
-
